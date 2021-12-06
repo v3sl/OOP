405e38de35554e2c6a6b6dc962c1db8273b9cc1a
Sasha Videvich <fpm.videvich@bsu.by>
2021-12-06 16:25:36 +0300

    added methods

--- stack_on_array.h
+++ stack_on_array.h
@@ -10,6 +10,11 @@ public:
 	Stack () {top = 0;}
 	void push (int var) {st [++top] = var;}
 	int pop () {return st [top--];}
+	bool IsEmpty(){return top;}
+	void print(){
+		for(int i = 1; i <= top; i++)
+			std::cout << st[i] << '\n';
+	}
 };
 
 #endif //OOP__STACK_ON_ARRAY_H_
