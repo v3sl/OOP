ed42c560159101b373703a83f6cc335ad9beb5c8
Sasha Videvich <fpm.videvich@bsu.by>
2021-12-06 16:29:25 +0300

    fixed IsEmpty

--- stack_on_array.h
+++ stack_on_array.h
@@ -5,12 +5,13 @@ class Stack{
 private:
 	enum {MAX = 10};
 	int st[MAX];
-	int top;
+
 public:
+	int top;
 	Stack () {top = 0;}
 	void push (int var) {st [++top] = var;}
 	int pop () {return st [top--];}
-	bool IsEmpty(){return top;}
+	bool IsEmpty(){return !top;}
 	void print(){
 		for(int i = 1; i <= top; i++)
 			std::cout << st[i] << '\n';
