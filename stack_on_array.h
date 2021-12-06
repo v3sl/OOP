1455d1b3042a1f501eb425f0465888febe713ff8
Sasha Videvich <fpm.videvich@bsu.by>
2021-12-06 16:42:15 +0300

    added methods

--- stack_on_array.h
+++ stack_on_array.h
@@ -4,16 +4,27 @@
 class Stack{
 private:
 	enum {MAX = 10};
-	int st[MAX];
-
+	int* st = new int[MAX];
 public:
-	int top;
-	Stack () {top = 0;}
-	void push (int var) {st [++top] = var;}
-	int pop () {return st [top--];}
-	bool IsEmpty(){return !top;}
+	int Top;
+	Stack () {Top = 0;}
+	int top(){
+		if(IsEmpty())
+			throw std::runtime_error("Stack is empty");
+		return st[1];
+	}
+	void push (int var) {st [++Top] = var;}
+	int pop () {
+		if(IsEmpty())
+			throw std::runtime_error("Stack is empty");
+		int answer = st [Top--];
+		for(int i = 1 ; i <= Top; i++)
+			st[i] = st[i+1];
+		return answer;
+	}
+	bool IsEmpty(){return !Top;}
 	void print(){
-		for(int i = 1; i <= top; i++)
+		for(int i = 1; i <= Top; i++)
 			std::cout << st[i] << '\n';
 	}
 };
